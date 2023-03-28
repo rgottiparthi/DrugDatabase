f2a2734a716851280f9819a52cbda41d02a6e831
Rohith Gottiparthi <rohith.gottiparthi@gmail.com>
2023-03-28 19:05:16 -0400

    added sample table

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 import sqlite3
 
 conn = sqlite3.connect('drugData.db')
+
+
+conn.execute('''CREATE TABLE Reviews
+               (Username TEXT,
+                MovieID TEXT,
+                ReviewTime DATETIME,
+                Rating REAL,
+                Review TEXT)''')
