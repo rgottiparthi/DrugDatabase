e72eec18873c2d945b1873072b11e52a2516594e
Rohith Gottiparthi <rohith.gottiparthi@gmail.com>
2023-03-28 19:29:46 -0400

    Added interacts table

--- setup.py
+++ setup.py
@@ -4,26 +4,24 @@ conn = sqlite3.connect('drugData.db')
 
 
 conn.execute('''CREATE TABLE Drugs
-               (D_Name TEXT PRIMARY KEY,
+               (D_Name TEXT,
                 Basic_Description TEXT,
                 Adverse_Effect_Description TEXT)''')
 
-conn.execute('''CREATE TABLE Products
-               (P_Name TEXT PRIMARY KEY,
-                Cost REAL,
-                Form TEXT,
-                Manufacturer TEXT,
-                D_NAME TEXT NOT NULL
-                FOREIGN KEY (D_Name) REFERENCES Drugs (D_Name)
-                    ON DELETE CASCADE ON UPDATE NO ACTION)''')
-
 conn.execute('''CREATE TABLE Indications
-               (I_Name TEXT PRIMARY KEY,
+               (I_Name TEXT,
                 Severity TEXT,
                 Description TEXT)''')
 
 conn.execute('''CREATE TABLE User
-               (UserID TEXT PRIMARY KEY,
-                Age INTEGER,
+               (UserID TEXT,
+                Age INT,
                 Sex TEXT
                 Pregnancy TEXT)''')
+
+conn.execute('''CREATE TABLE interacts
+               (D_Name_1 TEXT,
+                D_Name_2 TEXT,
+                PRIMARY KEY(D_Name_1, D_Name_2)
+                FOREIGN KEY (D_Name_1) REFERENCES Drugs(D_Name),
+                FOREIGN KEY (D_Name_2) REFERENCES Drugs(D_Name))''')
