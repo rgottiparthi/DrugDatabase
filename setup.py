e7c288e101a5f78b52c90d56f78825570ebf6736
tztian <tinaziyatian@gmail.com>
2023-03-28 19:07:19 -0400

    Added Drugs table

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ import sqlite3
 conn = sqlite3.connect('drugData.db')
 
 
-conn.execute('''CREATE TABLE Reviews
-               (Username TEXT,
-                MovieID TEXT,
-                ReviewTime DATETIME,
-                Rating REAL,
-                Review TEXT)''')
+conn.execute('''CREATE TABLE Drugs
+               (D_Name TEXT,
+                Basic_Description TEXT,
+                Adverse_Effect_Description TEXT)''')
