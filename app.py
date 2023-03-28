12a875ece5de0b381c3cfd5e283788be9e2b25c0
Rohith Gottiparthi <rohith.gottiparthi@gmail.com>
2023-03-28 18:47:01 -0400

    Created a flask homepage

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, request
 from socket import *
 import sqlite3 as sql
+
+def flaskUpdate():
+   s= socket(AF_INET, SOCK_STREAM)
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+   app.run(debug = True)
