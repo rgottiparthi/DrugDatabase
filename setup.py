ceefd3473d31c053242536cc621b9a9fb142d780
tztian <tinaziyatian@gmail.com>
2023-03-28 19:11:06 -0400

    added indications and user tables

--- setup.py
+++ setup.py
@@ -7,3 +7,14 @@ conn.execute('''CREATE TABLE Drugs
                (D_Name TEXT,
                 Basic_Description TEXT,
                 Adverse_Effect_Description TEXT)''')
+
+conn.execute('''CREATE TABLE Indications
+               (I_Name TEXT,
+                Severity TEXT,
+                Description TEXT)''')
+
+conn.execute('''CREATE TABLE User
+               (UserID TEXT,
+                Age INT,
+                Sex TEXT
+                Pregnancy TEXT)''')
