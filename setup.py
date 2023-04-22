97fb53d2aac2c7c9401c812a3bf968ddab7c8311
tztian <tinaziyatian@gmail.com>
2023-04-22 15:20:12 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('drugData.db')
+conn = sqlite3.connect(':memory:')
 cur = conn.cursor()
 
 # drugs table
@@ -8,12 +8,12 @@ cur.execute('''CREATE TABLE Drugs
                (D_Name TEXT PRIMARY KEY,
                 Basic_Description TEXT,
                 Toxicity TEXT,
-                Indications TEXT,
-                Food_Interactions TEXT)''')
+                Indications TEXT)''')
 
 # products table
 cur.execute('''CREATE TABLE Products
-               (P_Name TEXT PRIMARY KEY,
+               (P_ID INTEGER PRIMARY KEY,
+                P_Name TEXT,
                 Form TEXT,
                 Strength TEXT,
                 Route TEXT,
@@ -27,23 +27,26 @@ cur.execute('''CREATE TABLE Products
                     ON DELETE CASCADE ON UPDATE NO ACTION)''')
 
 cur.execute('''CREATE TABLE Indications
-               (I_Name TEXT,
-                Description TEXT,
-                    ON DELETE CASCADE ON UPDATE NO ACTION)''')
+               (I_Name TEXT PRIMARY KEY,
+                Description TEXT)''')
 
 cur.execute('''CREATE TABLE Treats
                (D_Name TEXT,
                 I_Name TEXT,
-                PRIMARY KEY(D_Name_1, D_Name_2),
-                FOREIGN KEY (D_Name_1)
+                PRIMARY KEY(D_Name, I_Name),
+                FOREIGN KEY (D_Name) REFERENCES Drugs (D_Name)
                     ON DELETE CASCADE ON UPDATE NO ACTION,
-                FOREIGN KEY (I_Name_2)
+                FOREIGN KEY (I_Name) REFERENCES Indications (I_Name)
                     ON DELETE CASCADE ON UPDATE NO ACTION)''')
 
 cur.execute('''CREATE TABLE User
-               (Username TEXT PRIMARY KEY,
+               (UserID TEXT PRIMARY KEY,
                 Age INT,
-                Sex TEXT)''')
+                Sex TEXT,
+                Pregnancy TEXT,
+                Indication1 TEXT,
+                Indication2 TEXT,
+                Indication3 TEXT)''')
 
 cur.execute('''CREATE TABLE Interacts
                (D_Name_1 TEXT,
@@ -64,50 +67,65 @@ cur.execute('''CREATE TABLE Takes
 cur.execute('''CREATE TABLE Has
                (UserID TEXT,
                 I_Name TEXT,
-                FOREIGN KEY (UserID)
+                PRIMARY KEY(UserID, I_Name),
+                FOREIGN KEY (UserID) REFERENCES User (UserID)
                     ON DELETE CASCADE ON UPDATE NO ACTION,
-                FOREIGN KEY (I_Name)
+                FOREIGN KEY (I_Name) REFERENCES Indications (I_Name)
                     ON DELETE CASCADE ON UPDATE NO ACTION)''')
 
 
 import xml.etree.ElementTree as et
+import csv
 
 # Parsing the XML file
-tree = et.parse('full_database.xml')
+tree = et.parse("C:\\Users\\tinatian\\Documents\\full_database.xml")
 root = tree.getroot()
 
+# importing indications.csv file
+indications_file = open("C:\\Users\\tinatian\\Documents\\indications.csv")
+contents = csv.reader(indications_file)
+cur.executemany("INSERT INTO Indications (I_Name, Description) VALUES (?, ?)", contents)
+conn.commit()
+
 # drugs data
+n = 0
 for drug in root:
-    name = drug.find("{http://www.drugbank.ca}name").text
-    desc = drug.find("{http://www.drugbank.ca}description").text
-    toxicity = drug.find("{http://www.drugbank.ca}toxicity").text
-    # ADD INDICATIONS
-    cur.execute('''INSERT INTO Drugs (D_Name, Basic_Description, Toxicity) 
-                    VALUES (?, ?)''', (name, desc, toxicity))
+    if n < 10:
+        name = drug.find("{http://www.drugbank.ca}name").text
+        desc = drug.find("{http://www.drugbank.ca}description").text
+        toxicity = drug.find("{http://www.drugbank.ca}toxicity").text
+        indication = drug.find("{http://www.drugbank.ca}indication").text
+        cur.execute('''INSERT INTO Drugs (D_Name, Basic_Description, Toxicity, Indications) 
+                        VALUES (?, ?, ?, ?)''', (name, desc, toxicity, indication))
+        cur.execute("INSERT INTO Treats (D_Name) VALUES (?)", (name,))
+    n += 1
 conn.commit()
  
 
 # products data
 n = 0
+productID = 0
 for drug in root:
     if n < 10:
         name = drug.find("{http://www.drugbank.ca}name").text
         products = drug.find("{http://www.drugbank.ca}products")
         for product in products:
-            p_name = product.find("{http://www.drugbank.ca}name").text
-            p_labeller = product.find("{http://www.drugbank.ca}labeller").text
-            p_marketing_start = product.find("{http://www.drugbank.ca}started-marketing-on").text
-            p_marketing_end = product.find("{http://www.drugbank.ca}ended-marketing-on").text
-            p_dosage = product.find("{http://www.drugbank.ca}dosage-form").text
-            p_strength = product.find("{http://www.drugbank.ca}strength").text
-            p_route = product.find("{http://www.drugbank.ca}route").text
-            p_generic = product.find("{http://www.drugbank.ca}generic").text
-            p_otc = product.find("{http://www.drugbank.ca}over-the-counter").text
-
-            cur.execute('''INSERT INTO Products (P_Name, Form, Strength, Route, Manufacturer, 
-                            Marketing_start, Marketing_end, Generic, OTC, D_NAME) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                            (p_name, p_dosage, p_strength, p_route, p_labeller, p_marketing_start, p_marketing_end,
-                            p_generic, p_otc, name))
+            country = product.find("{http://www.drugbank.ca}country").text
+            if(country == 'US'):
+                p_name = product.find("{http://www.drugbank.ca}name").text
+                p_labeller = product.find("{http://www.drugbank.ca}labeller").text
+                p_marketing_start = product.find("{http://www.drugbank.ca}started-marketing-on").text
+                p_marketing_end = product.find("{http://www.drugbank.ca}ended-marketing-on").text
+                p_dosage = product.find("{http://www.drugbank.ca}dosage-form").text
+                p_strength = product.find("{http://www.drugbank.ca}strength").text
+                p_route = product.find("{http://www.drugbank.ca}route").text
+                p_generic = product.find("{http://www.drugbank.ca}generic").text
+                p_otc = product.find("{http://www.drugbank.ca}over-the-counter").text
+                cur.execute('''INSERT INTO Products (P_ID, P_Name, Form, Strength, Route, Manufacturer, 
+                                Marketing_start, Marketing_end, Generic, OTC, D_NAME) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                                (productID, p_name, p_dosage, p_strength, p_route, p_labeller, p_marketing_start, p_marketing_end,
+                                p_generic, p_otc, name))
+                productID += 1
     n += 1
 conn.commit()
 
@@ -127,7 +145,13 @@ for drug in root:
 conn.commit()
 
 
+
 res = cur.execute("SELECT * FROM Drugs")
 rec = res.fetchall()
 for x in range(3):
     print(rec[x], "\n")
+
+res2 = cur.execute("SELECT * FROM Indications")
+rec2 = res.fetchall()
+for x in range(3):
+    print(rec2[x], "\n")
