783c034be980f9dc773b048d01046360e154bc0e
tztian <tinaziyatian@gmail.com>
2023-04-23 21:07:49 -0400

    added graph to app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from socket import *
 import sqlite3 as sql
+import plotly.graph_objs as go
+import plotly.offline as pyo
+import networkx as nx
 
 def flaskUpdate():
    s= socket(AF_INET, SOCK_STREAM)
@@ -178,7 +181,75 @@ def graph():
          drug_name = request.form.get(f"Drug {i+1} Name")
          if drug_name:
                drugs.append(drug_name)
-      return render_template("interactionsGraph.html")
+         else:
+            drugs.append(None)
+      
+      # Connect to the SQLite database
+      conn = sql.connect('drugData.db')
+
+      # Execute SQL query to retrieve drug interaction data
+      cur = conn.cursor()
+      #cur.execute('SELECT D_Name_1, D_Name_2, I_Description FROM Interacts WHERE D_Name_1 = ? LIMIT 10', ("Ibuprofen",))
+      cur.execute('''SELECT D_Name_1, D_Name_2, I_Description
+                     FROM Interacts
+                     WHERE (D_Name_1 IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                     AND D_Name_2 IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                     AND D_Name_1 < D_Name_2)''', (drugs[0], drugs[1], drugs[2], drugs[3], drugs[4], drugs[5], drugs[6], drugs[7], drugs[8], drugs[9], drugs[0], drugs[1], drugs[2], drugs[3], drugs[4], drugs[5], drugs[6], drugs[7], drugs[8], drugs[9]))
+
+
+      # Fetch the results and create a list of edges
+      interactions = cur.fetchall()
+
+      # Create the graph
+      G = nx.Graph()
+      for interaction in interactions:
+         D_Name_1, D_Name_2, description = interaction
+         G.add_edge(D_Name_1, D_Name_2, description=description)
+
+      # Close the database connection
+      conn.close()
+
+      # Define node positions for the graph layout
+      pos = nx.spring_layout(G)
+
+      # Create a list of edge trace objects with descriptions
+      edge_trace = []
+      for edge in G.edges():
+         x0, y0 = pos[edge[0]]
+         x1, y1 = pos[edge[1]]
+         trace = go.Scatter(x=[x0, x1, None], y=[y0, y1, None],
+                           mode='lines',
+                           line=dict(width=10),
+                           hoverinfo='text', # set hoverinfo to "text"
+                           hovertext=G[edge[0]][edge[1]]['description']) # set hovertext to description
+         edge_trace.append(trace)
+
+      # Create a list of node trace objects
+      node_trace = go.Scatter(x=[], y=[], text=[], mode='markers', hoverinfo='text',
+                              marker=dict(color='black', size=10, line=dict(width=2)))
+
+      # Add node positions and labels to the node trace
+      for node in G.nodes():
+         x, y = pos[node]
+         node_trace['x'] += tuple([x])
+         node_trace['y'] += tuple([y])
+         node_trace['text'] += tuple([node])
+
+      # Create the figure object
+      fig = go.Figure(data=edge_trace + [node_trace],
+                     layout=go.Layout(
+                        title='<br>Drug Interaction Graph',
+                        titlefont_size=16,
+                        showlegend=True,
+                        hovermode='closest',
+                        margin=dict(b=20, l=5, r=5, t=40),
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
+
+      # Display the figure
+      pyo.plot(fig, filename='drug_interaction_graph.html')
+
+      return render_template("drug_interaction_graph.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
